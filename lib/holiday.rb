580fd73e9b828b533542b01156f1cd0b9be89dea
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-24 02:26:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,9 +71,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  arr = []
   holiday_hash.each { |season, holiday|
     if holiday.values.include?("BBQ")
-      holiday
+      array << holiday
     end
   }
 end
